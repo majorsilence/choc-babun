071a9f5c191645091e2d0fdd86fe65f24547105e
Peter Gill <peter@majorsilence.com>
2015-07-11 15:14:56 -0230

    Use Install-ChocolateyZipPackage

--- package/tools/chocolateyInstall.ps1
+++ package/tools/chocolateyInstall.ps1
@@ -1,46 +1,23 @@
-$package = 'babun'
-
-# ------------------------
-#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+$packageName = 'babun'
 
 function RemoveOldBabun
 {
   $babunpath = Join-Path $env:USERPROFILE ".babun"
   if (Test-Path $babunpath)
   {
-    Remove-Item -Recurse -Force $babunpath 
+    Remove-Item -Recurse -Force $babunpath
   }
 }
 
-$packageName = $package;
-$url = 'http://projects.reficio.org/babun/babun-dist.zip';
-
-try {
-
-
-
-  $chocTempDir = Join-Path $env:TEMP "chocolatey"
-  $tempDir = Join-Path $chocTempDir "$packageName" 
-
-  $setupBat = Join-Path $tempDir "babun-1.0.1"
-  $setupBat = Join-Path $setupBat "install.bat"
-
-  $zipFile = "babun.zip"
-  
-  $tempDownloadFolder 
-  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
-        
-  $file = Join-Path $tempDir $zipFile
-  Get-ChocolateyWebFile $packageName $file $url
-  Get-ChocolateyUnzip $file $tempDir "" $packageName
-  
-  RemoveOldBabun
-  start-process $setupBat
-
-
-   # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+function RunBabunInstall($unzipPath)
+{
+    RemoveOldBabun
+    $setupBat = Join-Path $unzipPath "babun"
+    $setupBat = Join-Path $unzipPath "install.bat"
+    start-process $setupBat
 }
+
+$url = 'http://projects.reficio.org/babun/download';
+$unzipPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+Install-ChocolateyZipPackage $packageName $url $unzipPath
+RunBabunInstall $unzipPath
